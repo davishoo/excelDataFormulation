a56fd7e7d2d1e35cc7b68ba8d785ec2039a44061
Davis <50162710+davishoo@users.noreply.github.com>
2019-05-27 18:19:01 +0800

    Add files via upload

--- outputQuotation.py
+++ outputQuotation.py
@@ -1,4 +1,4 @@
-import openpyxl, pprint, logging, time
+import openpyxl, pprint, logging, time, sys
 from openpyxl.styles import Font
 from datetime import datetime
 
@@ -10,9 +10,15 @@ try:
     print('Import database module: [hotelQuotation] ...')
 except  ModuleNotFoundError:
     print('warning: no hotelQuotation module found!')
+    sys.exit()
 
-#Add more attributes to database
 
+try:
+    from hotelDatabase import hotelDB
+    print('Import database module: [hotelDatabase] ...')
+except:
+    print('warning: no hotelDatabase module found!')
+    sys.exit()
 
 
 wb = openpyxl.Workbook()
@@ -20,6 +26,18 @@ sheet = wb['Sheet']
 
 
 #fill out the head of the sheet
+rowNum = 2
+
+for project, v1 in quotationData.keys()    
+    for quotationSource, v2 in quotationData[
+        sheet.cell(row = rowNum, column = 1).value = quotationSource
+        for hotel in quotation[project][quotationSource]            
+            sheet.cell(row = rowNum, column = 1).value = hotelDB[hotel][-1]['hotelLocation']
+            sheet.cell(row = rowNum, column = 2).value = hotel
+            for roomType in quotation[project][quotationSource][hotel]
+                sheet.cell(row = rowNum, column = 4).value = roomType
+                for quoteDate in quotation[project][quotationSource][hotel][roomType]
+                    sheet.cell(row = rowNum, column = 5).value = quoteDate
 
 
 sheet['A1'] = 'hello'
